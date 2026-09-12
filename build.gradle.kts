2a74d4aefdcff646382a7f7b090ff5f2fa829f0d
xechoz.zheng <xechoz.zheng@gmail.com>
2026-09-13 00:43:22 +0800

    Set up Maven Central publishing for the loga library
    Add the vanniktech maven-publish plugin, group/version coordinates, POM metadata, and the Apache 2.0 LICENSE.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.composeMultiplatform) apply false
     alias(libs.plugins.composeCompiler) apply false
+    alias(libs.plugins.mavenPublish) apply false
 }
--- loga/build.gradle.kts
+++ loga/build.gradle.kts
@@ -4,7 +4,11 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidKotlinMultiplatformLibrary)
+    alias(libs.plugins.mavenPublish)
 }
+
+group = "io.github.xechoz"
+version = providers.gradleProperty("loga.version").getOrElse("0.0.1")
 
 kotlin {
     jvm {
@@ -36,6 +40,38 @@ kotlin {
         }
         commonTest.dependencies {
             implementation(kotlin("test"))
+        }
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+
+    coordinates(group.toString(), "loga", version.toString())
+
+    pom {
+        name.set("loga")
+        description.set("A Kotlin Multiplatform logging library backed by mmap.")
+        url.set("https://github.com/xechoz/Loga")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("repo")
+            }
+        }
+        developers {
+            developer {
+                id.set("xechoz")
+                name.set("xechoz")
+                url.set("https://github.com/xechoz")
+            }
+        }
+        scm {
+            url.set("https://github.com/xechoz/Loga")
+            connection.set("scm:git:git://github.com/xechoz/Loga.git")
+            developerConnection.set("scm:git:ssh://git@github.com/xechoz/Loga.git")
         }
     }
 }
